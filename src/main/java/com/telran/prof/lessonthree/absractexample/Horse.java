9efdd10c2df46cbbb4319bbf6d6eee9b1dd810e0
IuriiAv <avramenko.ca@gmail.com>
2025-01-10 00:00:38 +0300

    add lessonprof three

---
+++ src/main/java/com/telran/prof/lessonthree/absractexample/Horse.java
@@ -0,0 +1,19 @@
+package com.telran.prof.lessonthree.absractexample;
+
+public class Horse extends EarthAnimal{
+
+    @Override
+    public void sleep() {
+
+    }
+
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public void walk() {
+
+    }
+}
