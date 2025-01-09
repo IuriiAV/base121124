50a1962910417adac7f49e42430dcac7b9fc37b0
KarpovYehor <karpovYehor@gmail.com>
2025-01-09 23:00:20 +0200

    Lesson3 with Abstract with interface

---
+++ src/main/java/com/telran/prof/lessons/lessons3/abstractexample/Fish.java
@@ -0,0 +1,19 @@
+package com.telran.prof.lessons.lessons3.abstractexample;
+
+public class Fish extends WaterAnimal {
+
+    @Override
+    public void swim() {
+
+    }
+
+    @Override
+    public void sleep() {
+
+    }
+
+    @Override
+    public void eat() {
+
+    }
+}
---
+++ src/main/java/com/telran/prof/lessons/lessons3/abstractexample/Horse.java
@@ -0,0 +1,19 @@
+package com.telran.prof.lessons.lessons3.abstractexample;
+
+public class Horse extends EarthAnimal{
+
+    @Override
+    public void walk() {
+
+    }
+
+    @Override
+    public void sleep() {
+
+    }
+
+    @Override
+    public void eat() {
+
+    }
+}
