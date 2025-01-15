592c999065527efffb0b9c3d853620fc87ae2cc5
KarpovYehor <karpovYehor@gmail.com>
2025-01-15 17:05:55 +0200

    Homework 3

---
+++ src/main/java/com/telran/prof/lessons/lesson3/Homework3/RadioPhone.java
@@ -0,0 +1,26 @@
+package com.telran.prof.lessons.lesson3.Homework3;
+
+public class RadioPhone extends Phone {
+
+    private String ownNumber;
+
+    public String getOwnNumber() {
+        return ownNumber;
+    }
+
+    public void setOwnNumber(String ownNumber) {
+        this.ownNumber = ownNumber;
+    }
+
+    @Override
+    public void call(String number) {
+        System.out.print("The phone: " + getPhoneName() + " " + ownNumber + " ");
+        super.call(number);
+    }
+
+    @Override
+    public void receiveCall(String number) {
+        System.out.print("The phone: " + getPhoneName() + " " + ownNumber + " ");
+        super.receiveCall(number);
+    }
+}
