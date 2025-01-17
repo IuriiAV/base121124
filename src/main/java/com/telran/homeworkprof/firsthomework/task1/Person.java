7165e0bb264491d440e1b3bc95a8a65acf8874d6
Nazatr <titan777151@gmail.com>
2025-01-17 15:58:35 +0200

    my commit

---
+++ src/main/java/com/telran/homeworkprof/firsthomework/task1/Person.java
@@ -0,0 +1,42 @@
+package com.telran.homeworkprof.firsthomework.task1;
+
+/**
+ * 1.Класс Person
+ * Создать класс Person, который содержит:
+ *
+ *
+ * переменные fullName, age; +
+ * методы move() и talk(), в которых просто вывести на консоль сообщение -"Такой-то <имя> говорит"
+ * и -"Такой-то <имя> идет"
+ * Добавьте два конструктора  - Person() и Person(fullName, age).
+ * Создайте два объекта этого класса. Один объект инициализируется конструктором Person(), другой - Person(fullName, age)
+ *
+ *
+ * Вызовите у объектов методы move и talk
+ *
+ *
+ */
+public class Person {
+
+    private String fullName;
+
+    private int age;
+
+
+    public Person() {
+
+    }
+
+    public Person(String fullName, int age) {
+        this.fullName = fullName;
+        this.age = age;
+    }
+
+    public void move(){
+        System.out.println("Person with name : " + fullName + " going ");
+    }
+
+    public void talk(){
+        System.out.println("Person with name : " + fullName + " talking ");
+    }
+}
---
+++ src/main/java/com/telran/homeworkprof/firsthomework/task2/Phone.java
@@ -0,0 +1,47 @@
+package com.telran.homeworkprof.firsthomework.task2;
+
+/**
+ *  * 2.Класс Phone.
+ *  * Создайте класс Phone, который содержит переменные number, model и weight.
+ *  * Создайте три экземпляра этого класса.
+ *  * Выведите на консоль значения их переменных.
+ *  * Добавить в класс Phone методы: receiveCall, имеет один параметр – имя звонящего.
+ *  * Выводит на консоль сообщение “Звонит {name}”. Метод getNumber – возвращает номер телефона.
+ *  * Вызвать эти методы для каждого из объектов.
+ **/
+public class Phone {
+
+    private String number;
+
+    private String model;
+
+    private int weight;
+
+    public Phone(String number, String model, int weight) {
+        this.number = number;
+        this.model = model;
+        this.weight = weight;
+    }
+
+    public void recieveCall(String nameWhoCalls) {
+        System.out.println("Calling " + nameWhoCalls);
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public static void main(String[] args) {
+        Phone phone1 = new Phone("+49324598294", "Samsung", 180);
+        Phone phone2 = new Phone("+49249944922", "Xiaomi", 200);
+        Phone phone3 = new Phone("+49929950029", "Iphone", 170);
+
+        Phone[] phones = {phone1, phone2, phone3};
+        for (Phone phone : phones) {
+            System.out.println("We have " + phone.model + " with number " + phone.number + " which has weight " + phone.weight);
+            System.out.println("Phone number that we get " + phone.getNumber());
+            phone.recieveCall("Petro");
+            System.out.println("-------------------");
+        }
+    }
+}
